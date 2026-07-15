7f8df5134d0b33d9811b8861892281764c36b29e
dharanipathi <153063045+dharanipath@users.noreply.github.com>
2026-07-15 16:14:20 +0530

    Delete email.generator/src/main/java/com/gemini/email/generator/DTO directory

--- email.generator/src/main/java/com/gemini/email/generator/DTO/EmailRequest.java
+++
@@ -1,5 +0,0 @@
-package com.gemini.email.generator.DTO;
-
-public record EmailRequest(String emailContent,String tone) {
-    
-}
--- email.generator/src/main/java/com/gemini/email/generator/DTO/EmailResponse.java
+++
@@ -1,5 +0,0 @@
-package com.gemini.email.generator.DTO;
-
-public record EmailResponse(String response) {
-    
-}
